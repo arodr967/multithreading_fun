1720743e338fe28314f16ec7fece0ce400d5555a
Alicia Rodriguez Taboada <alicia_rodriguez@ultimatesoftware.com>
2017-07-07 22:59:53 -0400

    update with appropriate pragmas for sync

--- part_2/task2_4.c
+++ part_2/task2_4.c
@@ -7,7 +7,7 @@
 
 int main(int argc, char *argv) {
   //set number of threads here
-  omp_set_num_threads(8);
+  omp_set_num_threads(32);
 
   int i, j, k;
   double sum;
@@ -36,14 +36,21 @@ int main(int argc, char *argv) {
 
   start = omp_get_wtime();
 
-  for (i = 0; i < M; i++) {
-    for (j = 0; j < N; j++) {
-       sum = 0;
-       for (k=0; k < M; k++) {
-          sum += A[i][k]*B[k][j];
-       }
-       C[i][j] = sum;
-   }
+  #pragma omp parallel shared(A,B,C,sum) private(i,j,k) // NOTE: Comment out this line to run parallelization for both loops (outer and middle)
+  {
+    // #pragma omp parallel for // NOTE: To run parallelization for both loops (outer and middle)
+    #pragma omp for // NOTE: To run parallelization for outer loop
+    for (i = 0; i < M; i++) {
+      // #pragma omp parallel for // NOTE: To run parallelization for both loops (outer and middle)
+      // #pragma omp for // NOTE: To run parallelization for inner loop
+      for (j = 0; j < N; j++) {
+         sum = 0;
+         for (k=0; k < M; k++) {
+            sum += A[i][k]*B[k][j];
+         }
+         C[i][j] = sum;
+     }
+    }
   }
 
   end = omp_get_wtime();
